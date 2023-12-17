fc4deb9752f6454d4b90d16a19e4984114e36d76
Promise Rijal <rpromise21@tbc.edu.np>
2023-12-17 18:26:27 +0545

    changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://ec2-3-90-45-205.compute-1.amazonaws.com:5000', // Replace with the URL of your Express backend
+      target: 'http://ec2-54-221-99-70.compute-1.amazonaws.com:5000/', // Replace with the URL of your Express backend
       changeOrigin: true,
     })
   );
